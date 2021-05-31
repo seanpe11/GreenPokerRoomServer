215458b35fdb94367ae6c5cdf8c0a5a69ef4428f
Jamalul, Gabriel Rasheed <gabriel_jamalul@dlsu.edu.ph>
2021-05-31 22:02:37 +0800

    fixed check

--- yeehawholdem.js
+++ yeehawholdem.js
@@ -116,7 +116,11 @@ class Yeehaw {
                         this.nextphase();
                         return { result: "CHECK", isValid: true, playerIndex: action.playerIndex, value: action.value };
                     } else if (this.currentBet == 0 ) {
-                            this.nextturn();
+                        this.toact = this.notfolded[(this.notfolded.indexOf(this.toact) + 1) % this.notfolded.length]
+                        if (this.toact == this.lastbet) // TODO: condition when bet has been matched
+                        { 
+                            this.nextphase();
+                        } 
                         return { result: "CHECK", isValid: true, playerIndex: action.playerIndex, value: action.value };
                     } else if(action.playerIndex != this.bigblind && this.currentBet != 0 )
                     {
